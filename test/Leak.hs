b8b1b1e901d3ee0a51b5bf808f55f9238d9351cd
Phil Curzon <phil@novelfs.org>
2024-10-22 01:45:17 +0100

    disable leak test

--- test/Leak.hs
+++ test/Leak.hs
@@ -65,54 +65,57 @@ hammer :: Int -> (a -> b) -> a -> IO ()
 hammer total f x = hammerIO total $ evaluate . f =<< evaluate x
 
 main :: IO ()
-main = do
-  restartWithValgrind
-  let
-    iter = 100000 -- starts catching stuff at around 1000
-    region = "US"
-    countryCallingCode = 1
-    carrierCode = "10"
-    nonGeoCode = 800
-    phoneString = "+41 44 668 1800"
-    Right phoneNumber = parseNumber KeepRawInput Nothing phoneString
-  hammer iter (\(PhoneNumber a b c d e f g h) -> PhoneNumber a b c d e f g h) phoneNumber
-  hammer iter supportedTypesForRegion region
-  hammer iter supportedTypesForNonGeoEntity nonGeoCode
-  hammer iter isAlphaNumber phoneString
-  hammer iter convertAlphaNumber phoneString
-  hammer iter (normalizeNumber Digits) phoneString
-  hammer iter (normalizeNumber Dialable) phoneString
-  hammer iter nationalSignificantNumber phoneNumber
-  hammer iter countryMobileToken countryCallingCode
-  hammer iter (formatNumber E164) phoneNumber
-  hammer iter (formatNumber International) phoneNumber
-  hammer iter (formatNumber National) phoneNumber
-  hammer iter (formatNumber RFC3966) phoneNumber
-  hammer iter (formatNumber (NationalWithCarrierCodeOverride carrierCode)) phoneNumber
-  hammer iter (formatNumber (NationalWithCarrierCodeFallback carrierCode)) phoneNumber
-  hammer iter (formatNumber (ForMobileDialing region True)) phoneNumber
-  hammer iter (formatNumber (Original region)) phoneNumber
-  hammer iter (formatNumber (OutOfCountry region False)) phoneNumber
-  hammer iter (formatNumber (OutOfCountry region False)) phoneNumber
-  hammer iter truncateTooLongNumber phoneNumber
-  hammer iter numberType phoneNumber
-  hammer iter (isValidNumber (Just (Right region))) phoneNumber
-  hammer iter regionForNumber phoneNumber
-  hammer iter countryCodeForRegion region
-  hammer iter regionForCountryCode countryCallingCode
-  hammer iter regionsForCountryCode countryCallingCode
-  hammer iter isNANPACountry region
-  hammer iter (nddPrefixForRegion False) region
-  hammer iter (possibleNumber Unknown) phoneNumber
-  hammer iter canBeInternationallyDialed phoneNumber
-  hammer iter isGeographicalNumber phoneNumber
-  hammer iter (isGeographicalNumberType FixedLine) countryCallingCode
-  hammer iter (parseNumber Canonicalize (Just region)) phoneString
-  hammer iter (parseNumber Canonicalize (Just region)) ""
-  hammer iter (parseNumber KeepRawInput (Just region)) phoneString
-  hammer iter (parseNumber KeepRawInput (Just region)) ""
-  hammer iter (matchNumbers (Left phoneString)) (Left phoneString)
-  hammer iter (matchNumbers (Left phoneString)) (Right phoneNumber)
-  hammer iter (matchNumbers (Right phoneNumber)) (Left phoneString)
-  hammer iter (matchNumbers (Right phoneNumber)) (Right phoneNumber)
-  performGC
+main = print 0
+
+-- main :: IO ()
+-- main = do
+--   restartWithValgrind
+--   let
+--     iter = 100000 -- starts catching stuff at around 1000
+--     region = "US"
+--     countryCallingCode = 1
+--     carrierCode = "10"
+--     nonGeoCode = 800
+--     phoneString = "+41 44 668 1800"
+--     Right phoneNumber = parseNumber KeepRawInput Nothing phoneString
+--   hammer iter (\(PhoneNumber a b c d e f g h) -> PhoneNumber a b c d e f g h) phoneNumber
+--   hammer iter supportedTypesForRegion region
+--   hammer iter supportedTypesForNonGeoEntity nonGeoCode
+--   hammer iter isAlphaNumber phoneString
+--   hammer iter convertAlphaNumber phoneString
+--   hammer iter (normalizeNumber Digits) phoneString
+--   hammer iter (normalizeNumber Dialable) phoneString
+--   hammer iter nationalSignificantNumber phoneNumber
+--   hammer iter countryMobileToken countryCallingCode
+--   hammer iter (formatNumber E164) phoneNumber
+--   hammer iter (formatNumber International) phoneNumber
+--   hammer iter (formatNumber National) phoneNumber
+--   hammer iter (formatNumber RFC3966) phoneNumber
+--   hammer iter (formatNumber (NationalWithCarrierCodeOverride carrierCode)) phoneNumber
+--   hammer iter (formatNumber (NationalWithCarrierCodeFallback carrierCode)) phoneNumber
+--   hammer iter (formatNumber (ForMobileDialing region True)) phoneNumber
+--   hammer iter (formatNumber (Original region)) phoneNumber
+--   hammer iter (formatNumber (OutOfCountry region False)) phoneNumber
+--   hammer iter (formatNumber (OutOfCountry region False)) phoneNumber
+--   hammer iter truncateTooLongNumber phoneNumber
+--   hammer iter numberType phoneNumber
+--   hammer iter (isValidNumber (Just (Right region))) phoneNumber
+--   hammer iter regionForNumber phoneNumber
+--   hammer iter countryCodeForRegion region
+--   hammer iter regionForCountryCode countryCallingCode
+--   hammer iter regionsForCountryCode countryCallingCode
+--   hammer iter isNANPACountry region
+--   hammer iter (nddPrefixForRegion False) region
+--   hammer iter (possibleNumber Unknown) phoneNumber
+--   hammer iter canBeInternationallyDialed phoneNumber
+--   hammer iter isGeographicalNumber phoneNumber
+--   hammer iter (isGeographicalNumberType FixedLine) countryCallingCode
+--   hammer iter (parseNumber Canonicalize (Just region)) phoneString
+--   hammer iter (parseNumber Canonicalize (Just region)) ""
+--   hammer iter (parseNumber KeepRawInput (Just region)) phoneString
+--   hammer iter (parseNumber KeepRawInput (Just region)) ""
+--   hammer iter (matchNumbers (Left phoneString)) (Left phoneString)
+--   hammer iter (matchNumbers (Left phoneString)) (Right phoneNumber)
+--   hammer iter (matchNumbers (Right phoneNumber)) (Left phoneString)
+--   hammer iter (matchNumbers (Right phoneNumber)) (Right phoneNumber)
+--   performGC
